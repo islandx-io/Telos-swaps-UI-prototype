9bde1ac9879604f0429fe5d5fe5df0539f171d55
Rory Mapstone <rory.mapstone@gmail.com>
2020-10-26 19:43:20 +0200

    Privacy update

--- src/components/common/Privacy.vue
+++ src/components/common/Privacy.vue
@@ -3,13 +3,13 @@
     <div class="content d-flex flex-column align-items-start">
       <h1 class="align-self-center">Privacy Policy</h1>
       <div class="wrapper">
-        <h2>XNation</h2>
+        <h2>Telos Swaps</h2>
         <p>
-          At XNation, accessible from
-          <a :href="website" class="orange" target="_blank">xnation.io</a>, one
+          At Telos Swaps, accessible from
+          <a :href="website" class="orange" target="_blank">telosd.com</a>, one
           of our main priorities is the privacy of our visitors. This Privacy
           Policy document contains types of information that is collected and
-          recorded by XNation and how we use it.
+          recorded by Telos Swaps and how we use it.
         </p>
         <p>
           If you have additional questions or require more information about our
@@ -19,22 +19,22 @@
         <h3>GENERAL DATA PROTECTION REGULATION (GDPR)</h3>
         <p>We are a Data Controller of your information.</p>
         <p>
-          XNation legal basis for collecting and using the personal information
+          Telos Swaps legal basis for collecting and using the personal information
           described in this Privacy Policy depends on the Personal Information
           we collect and the specific context in which we collect the
           information:
         </p>
         <ul>
-          <li>XNation needs to perform a contract with you</li>
-          <li>You have given XNation permission to do so</li>
+          <li>Telos Swaps needs to perform a contract with you</li>
+          <li>You have given Telos Swaps permission to do so</li>
           <li>
-            Processing your personal information is in XNation legitimate
+            Processing your personal information is in Telos Swaps legitimate
             interests
           </li>
-          <li>XNation needs to comply with the law</li>
+          <li>Telos Swaps needs to comply with the law</li>
         </ul>
         <p>
-          XNation will retain your personal information only for as long as is
+          Telos Swaps will retain your personal information only for as long as is
           necessary for the purposes set out in this Privacy Policy. We will
           retain and use your information to the extent necessary to comply with
           our legal obligations, resolve disputes, and enforce our policies.
@@ -62,7 +62,7 @@
         </ul>
         <h2>Log Files</h2>
         <p>
-          XNation follows a standard procedure of using log files. These files
+          Telos Swaps follows a standard procedure of using log files. These files
           log visitors when they visit websites. All hosting companies do this
           as a part of hosting services’ analytics. The information collected by
           log files include internet protocol (IP) addresses, browser type,
@@ -74,7 +74,7 @@
         </p>
         <h2>COOKIES AND WEB BEACONS</h2>
         <p>
-          Like any other website, XNation uses ‘cookies’. These cookies are used
+          Like any other website, Telos Swaps uses ‘cookies’. These cookies are used
           to store information including visitors’ preferences, and the pages on
           the website that the visitor accessed or visited. The information is
           used to optimize the users’ experience by customizing our web page
@@ -83,24 +83,24 @@
         <h2>PRIVACY POLICIES</h2>
         <p>
           You may consult this list to find the Privacy Policy for each of the
-          advertising partners of XNation.
+          advertising partners of Telos Swaps.
         </p>
         <p>
           Third-party ad servers or ad networks uses technologies like cookies,
           JavaScript, or Web Beacons that are used in their respective
-          advertisements and links that appear on XNation, which are sent
+          advertisements and links that appear on Telos Swaps, which are sent
           directly to users’ browser. They automatically receive your IP address
           when this occurs. These technologies are used to measure the
           effectiveness of their advertising campaigns and/or to personalize the
           advertising content that you see on websites that you visit.
         </p>
         <p>
-          Note that XNation has no access to or control over these cookies that
+          Note that Telos Swaps has no access to or control over these cookies that
           are used by third-party advertisers.
         </p>
         <h2>THIRD PARTY PRIVACY POLICIES</h2>
         <p>
-          XNation’s Privacy Policy does not apply to other advertisers or
+          Telos Swaps’s Privacy Policy does not apply to other advertisers or
           websites. Thus, we are advising you to consult the respective Privacy
           Policies of these third-party ad servers for more detailed
           information. It may include their practices and instructions about how
@@ -120,7 +120,7 @@
           participate in, and/or monitor and guide their online activity.
         </p>
         <p>
-          XNation does not knowingly collect any Personal Identifiable
+          Telos Swaps does not knowingly collect any Personal Identifiable
           Information from children under the age of 13. If you think that your
           child provided this kind of information on our website, we strongly
           encourage you to contact us immediately and we will do our best
@@ -131,7 +131,7 @@
           This privacy policy (GDPR Privacy Policy created at
           GDPRPrivacyNotice.com) applies only to our online activities and is
           valid for visitors to our website with regards to the information that
-          they shared and/or collect in XNation. This policy is not applicable
+          they shared and/or collect in Telos Swaps. This policy is not applicable
           to any information collected offline or via channels other than this
           website.
         </p>
